ef2635b3ac518919efd9c16b1c311a0a947662af
Torsten Sommer <torsten.sommer@3ds.com>
2018-05-31 11:35:49 +0200

    Add tools [skip ci]

--- fmpy/cross_check/validate_vendor_repo.py
+++ fmpy/cross_check/validate_vendor_repo.py
@@ -121,7 +121,7 @@ def validate_test_fmu(model_dir):
     return problems
 
 
-def validate_cross_check_result(result_dir):
+def validate_cross_check_result(result_dir, tools):
     """ Validate a cross-check result
 
     Parameters:
@@ -131,19 +131,13 @@ def validate_cross_check_result(result_dir):
         a list of problems
     """
 
-    from fmpy.util import validate_result
+    # check if the import has claims 'passed' state
+    claims_passsed = os.path.isfile(os.path.join(result_dir, 'passed'))
+
+    if not claims_passsed:
+        return []  # skip this result
 
     problems = []
-
-    if os.path.isfile(os.path.join(result_dir, 'notCompliantWithLatestRules')):
-        return problems
-
-    # check ReadMe
-    if not os.path.isfile(os.path.join(result_dir, 'ReadMe.txt')) and not os.path.isfile(os.path.join(result_dir, 'ReadMe.pdf')):
-        problems.append("Readme.[txt|pdf] is missing in %s" % result_dir)
-
-    if not os.path.isfile(os.path.join(result_dir, 'passed')):
-        return problems
 
     path, model_name = os.path.split(result_dir)
     path, exporting_tool_version = os.path.split(path)
@@ -156,28 +150,153 @@ def validate_cross_check_result(result_dir):
     path, _ = os.path.split(path)
     path, vendor = os.path.split(path)
 
-    exporting_vendor = '3ds'
+    if exporting_tool_name not in tools:
+        problems.append('Unknown tool "%s"' % exporting_tool_name)
+        return problems
+
+    exporting_vendor = tools[exporting_tool_name]
+
+    # path to the FMU's directory
+    ref_dir = os.path.join(path, exporting_vendor, 'Test_FMUs', fmi_version, fmi_type, platform, exporting_tool_name, exporting_tool_version, model_name)
+
+    # check if the FMU complies to the XC rules
+    if os.path.isfile(os.path.join(ref_dir, 'doesNotComplyWithLatestRules')):
+        problems.append('The exported FMU does not comply with the cross-check rules')
+        return problems
 
     # check the output file
-    ref_csv_filename = os.path.join(path, exporting_vendor, 'Test_FMUs', fmi_version, fmi_type, platform, exporting_tool_name, exporting_tool_version, model_name, model_name + '_ref.csv')
+    ref_csv_filename = os.path.join(ref_dir, model_name + '_ref.csv')
 
-    if os.path.isfile(ref_csv_filename):
+    # check if the reference result exists
+    if not os.path.isfile(ref_csv_filename):
+        problems.append('The reference result "%s" was not found' % ref_csv_filename)
+        return problems
+
+    try:
+        # read the reference result
         ref = read_csv(ref_csv_filename)
-    else:
-        ref = None
+    except Exception as e:
+        problems.append("Error in %s. %s" % (ref_csv_filename, e))
+        return problems
 
     csv_filename = os.path.join(result_dir, model_name + '_out.csv')
 
     try:
-        out = read_csv(csv_filename)
+        # read the result
+        res = read_csv(csv_filename)
     except Exception as e:
-        out = None
         problems.append("Error in %s. %s" % (csv_filename, e))
+        return problems
 
-    if ref is not None and out is not None:
-        s = validate_result(result=out, reference=ref)
+    # validate the results
+    problem = validate_result(result=res, reference=ref)
+
+    if problem is not None:
+        problems.append('Error in %s: %s' % (csv_filename, problem))
 
     return problems
+
+
+def validate_result(result, reference, stop_time=None):
+    """ Validate a simulation result against a reference result
+
+    Parameters:
+        result      structured NumPy array where the first column is the time
+        reference   same as result
+
+    Returns:
+        problems    a list of problems
+    """
+
+    t_ref = reference[reference.dtype.names[0]]
+    t_res = result[result.dtype.names[0]]
+
+    # at least two samples are required
+    if result.size < 2:
+        return 'The result must have at least two samples'
+
+    # check if stop time has been reached
+    if stop_time is not None and t_res[-1] < stop_time:
+        return 'The stop time %g has not been reached'
+
+    # check if all reference signals are contained in the result
+    for name in reference.dtype.names[1:]:
+        if name not in result.dtype.names:
+            return 'Variable "%s" is missing' % name
+
+    rel_out = 0
+
+    # find the signal with the most outliers
+    for name in result.dtype.names[1:]:
+
+        if name not in reference.dtype.names:
+            continue
+
+        y_ref = reference[name]
+        y_res = result[name]
+        _, _, _, outliers = validate_signal(t=t_res, y=y_res, t_ref=t_ref, y_ref=y_ref)
+        rel_out = np.max([np.sum(outliers) / float(len(outliers)), rel_out])
+
+    # TODO: check rel_out
+
+    return None
+
+
+def validate_signal(t, y, t_ref, y_ref, num=1000, dx=20, dy=0.1):
+    """ Validate a signal y(t) against a reference signal y_ref(t_ref) by creating a band
+    around y_ref and finding the values in y outside the band
+
+    Parameters:
+
+        t       time of the signal
+        y       values of the signal
+        t_ref   time of the reference signal
+        y_ref   values of the reference signal
+        num     number of samples for the band
+        dx      horizontal width of the band in samples
+        dy      vertical distance of the band to y_ref
+
+    Returns:
+
+        t_band  time values of the band
+        y_min   lower limit of the band
+        y_max   upper limit of the band
+        i_out   indices of the values in y outside the band
+    """
+
+    from scipy.ndimage.filters import maximum_filter1d, minimum_filter1d
+
+    # re-sample the reference signal into a uniform grid
+    t_band = np.linspace(start=t_ref[0], stop=t_ref[-1], num=num)
+
+    # sort out the duplicate samples before the interpolation
+    m = np.concatenate(([True], np.diff(t_ref) > 0))
+
+    y_band = np.interp(x=t_band, xp=t_ref[m], fp=y_ref[m])
+
+    y_band_min = np.min(y_band)
+    y_band_max = np.max(y_band)
+
+    # calculate the width of the band
+    if y_band_min == y_band_max:
+        w = 0.5 if y_band_min == 0 else np.abs(y_band_min) * dy
+    else:
+        w = (y_band_max - y_band_min) * dy
+
+    # calculate the lower and upper limits
+    y_min = minimum_filter1d(input=y_band, size=dx) - w
+    y_max = maximum_filter1d(input=y_band, size=dx) + w
+
+    # find outliers
+    y_min_i = np.interp(x=t, xp=t_band, fp=y_min)
+    y_max_i = np.interp(x=t, xp=t_band, fp=y_max)
+    i_out = np.logical_or(y < y_min_i, y > y_max_i)
+
+    # do not count outliers outside the t_ref
+    i_out = np.logical_and(i_out, t > t_band[0])
+    i_out = np.logical_and(i_out, t < t_band[-1])
+
+    return t_band, y_min, y_max, i_out
 
 
 def segments(path):
@@ -196,14 +315,31 @@ def segments(path):
     return s
 
 
+def get_exporting_tools(repos_dir):
+    """ Collect the tools for all vendors """
+
+    tools = {}
+
+    for vendor in os.listdir(repos_dir):
+        for fmi_version in ['FMI_1.0', 'FMI_2.0']:
+            for fmi_type in ['CoSimulation', 'ModelExchange']:
+                for platform in ['darwin64', 'linux32', 'linux64', 'win32', 'win64']:
+                    platform_dir = os.path.join(repos_dir, vendor, 'Test_FMUs', fmi_version, fmi_type, platform)
+                    if os.path.isdir(platform_dir):
+                        for tool in os.listdir(platform_dir):
+                            tool_dir = os.path.join(platform_dir, tool)
+                            if os.path.isdir(tool_dir):
+                                tools[tool] = vendor
+
+    return tools
+
+
 if __name__ == '__main__':
 
     import argparse
     import textwrap
 
-    description = """\
-    Validate cross-check results and test FMUs in vendor repositories 
-    """
+    description = "Validate cross-check results and test FMUs in vendor repositories"
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
                                      description=textwrap.dedent(description))
@@ -219,6 +355,10 @@ if __name__ == '__main__':
         vendor_dir = args.repo_path
     else:
         vendor_dir = os.getcwd()
+
+    repos_dir, _ = os.path.split(vendor_dir)
+
+    tools = get_exporting_tools(repos_dir)
 
     s = segments(vendor_dir)
 
@@ -241,7 +381,7 @@ if __name__ == '__main__':
         if platform not in ['c-code', 'darwin64', 'linux32', 'linux64', 'win32', 'win64']:
             continue
 
-        problems += validate_cross_check_result(subdir)
+        problems += validate_cross_check_result(subdir, tools)
 
     # # validate the test FMUs
     # for subdir, dirs, files in os.walk(os.path.join(vendor_dir, 'Test_FMUs')):
@@ -263,6 +403,10 @@ if __name__ == '__main__':
     #         continue
     #
     #     problems += validate_test_fmu(subdir)
+
+
+    # filter Unknown...
+    problems = [p for p in problems if not p.startswith('Unknown')]
 
     print()
     print("#################################")
