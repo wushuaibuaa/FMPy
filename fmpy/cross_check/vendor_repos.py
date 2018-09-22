5341c858fc8edf48b699bcb4c626923c30a5d4de
Torsten Sommer <wakemaniac@gmail.com>
2018-09-23 00:43:34 +0200

    Add vendor repo cleanup [skip ci]

--- fmpy/cross_check/vendor_repos.py
+++ fmpy/cross_check/vendor_repos.py
@@ -1,15 +1,4 @@
-"""
-Clone or update the cross-check vendor repositories
-
-Clone all repositories into the current directory:
-    $ python clone_vendor_repos.py
-
-Clone 3ds and QTronic into /tmp/xc-data:
-    $ python clone_vendor_repos.py --destination /tmp/xc-data --vendors 3ds QTronic
-"""
-
-
-def clone_repos(path=None, vendors=None):
+def clone_repos(tools_csv, vendor_repos_dir):
     """
     Clone or update the cross-check repositories
     """
@@ -18,10 +7,7 @@ def clone_repos(path=None, vendors=None):
     from subprocess import call
     from fmpy.cross_check import get_vendor_ids
 
-    if path is None:
-        path = os.getcwd()
-
-    vendors = get_vendor_ids('/Users/tors10/Development/fmi-standard.org/_data/tools.csv')
+    vendors = get_vendor_ids(tools_csv)
 
     for vendor in sorted(vendors.keys()):
 
@@ -29,18 +15,22 @@ def clone_repos(path=None, vendors=None):
 
             clone_url = 'https://github.com/fmi-crosscheck/%s.git' % vendor
 
-            repo_dir = os.path.join(path, vendor)
+            repo_dir = os.path.join(vendor_repos_dir, vendor)
 
             try:
-                # pull from the remote
                 print('Pulling %s...' % vendor)
+                # revert all changed files
+                call(['git', 'reset', '--hard'], cwd=repo_dir)
+                # remove untracked files and directories
+                call(['git', 'clean', '-fd'], cwd=repo_dir)
+                # pull from origin
                 call(['git', 'lfs', 'pull'], cwd=repo_dir)
                 # TODO: reset --hard, clean -fd
             except Exception as e1:
                 # clone the repository if it doesn't exist yet
                 print('Cloning %s...' % vendor)
                 try:
-                    call(['git', 'clone', clone_url], cwd=path)
+                    call(['git', 'clone', clone_url], cwd=vendor_repos_dir)
                 except Exception as e2:
                     # catch problems like empty repo
                     print(e2)
@@ -54,10 +44,7 @@ if __name__ == '__main__':
 
     parser.add_argument('tools_csv', help="Path to the tools.csv file from the fmi-standard.org repository")
     parser.add_argument('vendor_repos_dir', help="Directory that contains the vendor repositories")
-    parser.add_argument('--vendors', nargs='+', help="Vendor repositories to clone")
 
     args = parser.parse_args()
 
-    clone_repos(tools_csv=args.tools_csv,
-                vendor_repos_dir=args.vendor_repos_dir,
-                vendors=args.vendors)
+    clone_repos(tools_csv=args.tools_csv, vendor_repos_dir=args.vendor_repos_dir)
